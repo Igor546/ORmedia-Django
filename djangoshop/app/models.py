0cafe0821d1ba218aa20354f94eeff8bdca18863
Igor <yudin-igor@rambler.ru>
2019-07-10 19:05:47 +0300

    Update 6 Создаем модель товара

--- djangoshop/app/models.py
+++ djangoshop/app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # python manage.py makemigrations
 # python manage.py migrate
 
+# Категория товара
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
@@ -13,9 +14,26 @@ class Category(models.Model):
         return self.name
 
 
+# Бренд товара
 class Brand(models.Model):
     name = models.CharField(max_length=100)
 
     # Чтобы в админке имя категории было не "Category object (1)" , а "self.name" (Смартфоны)
     def __str__(self):
         return self.name
+
+
+# Товар
+class Product(models.Model):
+    # ForeignKey. Внешний-ключ (у одного товара может быть только одна категория/бренд)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)  # (Всего цифр <=9, после запятой 2)
+    available = models.BooleanField(default=True)  # Товар Активен/Неактивен
+
+    def __str__(self):
+        return self.title
