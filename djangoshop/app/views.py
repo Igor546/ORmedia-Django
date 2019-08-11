49ddb10968d01bf4c0edfb85fb6c0d209c11c92b
Igor <yudin-igor@rambler.ru>
2019-08-11 20:27:06 +0300

    Update 12 Отображаем информацию о товарах и категориях

--- djangoshop/app/views.py
+++ djangoshop/app/views.py
@@ -25,7 +25,9 @@ def product_view(request, product_slug):
 # Просмотр по продуктам
 def category_view(request, category_slug):
     category = Category.objects.get(slug=category_slug)
+    products_of_category = Product.objects.filter(category=category)
     context = {
-        'category': category
+        'category': category,
+        'products_of_category': products_of_category
     }
     return render(request, 'category.html', context)
